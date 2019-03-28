e71e5c828347afd7c2d84927ebd41ba6ede19ad1
weathertopper <nathanweatherly94@gmail.com>
2019-03-28 15:27:13 -0400

    angband with no doc

---
+++ ANGBAND/setup_angband.sh
@@ -0,0 +1,13 @@
+#bin/bash
+
+ANGBAND_DIR=angband-4.1.3
+tar -xzf ${ANGBAND_DIR}.tar.gz
+cd $ANGBAND_DIR
+# sudo apt-get install autoconf gcc libc6-dev libncurses5-dev libx11-dev \
+#    libsdl1.2-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+
---
+++ ANGBAND/start_angband.sh
@@ -0,0 +1,6 @@
+#bin/bash
+
+ANGBAND_DIR=angband-4.1.3
+
+cd $ANGBAND_DIR
+angband
